508bcee56ca8efb2da09a2acff1f7945edb0722e
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-12-06 23:30:11 +0200

    first

--- apps/user-db/src/main.ts
+++ apps/user-db/src/main.ts
@@ -1,11 +1,6 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
@@ -13,10 +8,11 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 
-bootstrap();
+bootstrap().then();

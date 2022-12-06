395cb9ae6b508076e6a330a658b031192cabc477
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-12-07 00:22:43 +0200

    first

--- apps/user-db/src/app/app.module.ts
+++ apps/user-db/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true, envFilePath: '/.user-db.env'}),
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- apps/user-db/src/main.ts
+++ apps/user-db/src/main.ts
@@ -4,12 +4,21 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const application = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  application.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
 
-  await app.listen(port);
+  const config = new DocumentBuilder()
+    .setTitle('Accounts')
+    .setDescription('The Accounts API built on Monorepo')
+    .setVersion('1.0')
+    .addTag('accounts')
+    .build();
+  const document = SwaggerModule.createDocument(application, config);
+  SwaggerModule.setup('swagger', application, document);
+
+  await application.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );

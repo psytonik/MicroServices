127eb6c69cf4ad88b67f5224afb1d63a4661839e
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-12-07 00:24:07 +0200

    first

--- apps/user-db/src/app/app.module.ts
+++ apps/user-db/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true, envFilePath: '/.user-db.env'}),
+    ConfigModule.forRoot({isGlobal: true, envFilePath: '/envs/.user-db.env'}),
     UserModule,
     AuthModule,
   ],
